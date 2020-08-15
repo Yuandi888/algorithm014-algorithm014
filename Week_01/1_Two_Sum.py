5ce2bc608575dc94467320b4cfae629b065abe7e
Yuandi <karlnong@126.com>
2020-08-16 01:35:01 +0800

    1. Two Sum

---
+++ Week_01/1_Two_Sum.py
@@ -0,0 +1,24 @@
+# 1. Two Sum
+# 1. 两数之和
+
+
+# 暴力求解
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                    break
+
+
+
+# 通过哈希表
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i, num in enumerate(nums):
+            if hashmap.get(target - num) is not None:
+                return [i, hashmap.get(target - num)]
+            hashmap[num] = i
